0b3a4587efd17cf4e60d6b0d36127b0007f30e02
Karina Espinola <k.espinolah@gmail.com>
2021-08-01 20:07:47 -0600

    Add favicon and initial text

--- src/App.js
+++ src/App.js
@@ -9,19 +9,25 @@ function App() {
       >
           <div className="h-5" />
           <img  src="/assets/star.svg" alt="Cali Espinola" title="Cali Espinola" />
-        <div className="container mx-auto relative">
-          <div className="absolute top-20">
+        <div className="container mx-auto relative grid grid-cols-2 gap-4">
+          <div className="sm:mt-28">
             <h1 
               className="text-white text-7xl font-bold"
               >
                 Karina Espínola
             </h1>
             <h2 
-              className="text-white text-4xl"
+              className="text-white text-4xl mt-3"
               style={{fontFamily: ['Fira Mono', 'monospace']}}
             >
               Web Developer
             </h2>
+            <p className="text-white mt-3">
+              Hello! Welcome to my website, thank you for stopping by :) 
+              I'm a web developer located in El Salvador. I have been working as a web developer for 7 years now, 
+              I have been involved in front-end and back-end projects. I love the fact that I learn something
+              new everyday! It's the best feeling! If you want to contact me please feel free to e-mail me or drop a line through social media :)
+            </p>
           </div>
         </div>
       </div>
