4bd76e536a7e6e7f6ec268e204a6126bc9d75702
Karina Espinola <k.espinolah@gmail.com>
2021-08-03 17:03:36 -0600

    Add social icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './styles/styles.css';
+import Email from './components/icons/Email';
+import LinkedIn from './components/icons/LinkedIn';
+import GitHub from './components/icons/GitHub';
 
 function App() {
   return (
@@ -23,18 +26,20 @@ function App() {
               Web Developer
             </h2>
             <p className="text-white mt-3">
-              Hello! Welcome to my website, thank you for stopping by :) 
-              I'm a web developer located in El Salvador. I have been working as a web developer for 7 years now, 
-              I have been involved in front-end and back-end projects. I love the fact that I learn something
-              new everyday! It's the best feeling! If you want to contact me please feel free to e-mail me or drop a line through social media :)
+            Hello! Welcome to my website, thank you for stopping by :) I'm a web developer located in El Salvador. 
+            I have been working as a web developer for 7 years now, I have been involved in front-end and back-end projects. 
+            I love the fact that I learn something new every day! It's the best feeling! 
+            If you want to contact me please feel free to e-mail me or drop me a line through social media :)
             </p>
-            <div className="flex">
-              <a href="mailto:kl.spinolah@gmail.com">
-                <img 
-                  src="/assets/email.svg" 
-                  alt="Email"
-                  className="text-white" 
-                />
+            <div className="flex items-center mt-5">
+              <a href="mailto:kl.spinolah@gmail.com" className="mr-3">
+                <Email classes="text-white h-10 w-10" />
+              </a>
+              <a href="https://www.linkedin.com/in/karina-espinola/" className="mr-3">
+                <LinkedIn classes="text-white h-11 w-11" />
+              </a>
+              <a href="https://github.com/karinaespinola">
+                <GitHub classes="text-white h-10 w-10" />
               </a>
             </div>
           </div>
---
+++ src/components/icons/LinkedIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LinkedIn = ({classes}) => {
+    return (
+        <svg className={classes} stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 17V13.5V10" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M11 17V13.75M11 10V13.75M11 13.75C11 10 17 10 17 13.75V17" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M7 7.01L7.01 6.99889" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+    )
+}
+
+export default LinkedIn
