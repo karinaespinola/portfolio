c99d481c1b07cd53ec8837a01a300ba5b40bb71a
Karina Espinola <k.espinolah@gmail.com>
2021-12-31 15:45:10 -0600

    Add friends avatar images

---
+++ src/components/Technologies.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+        <div className="container mx-auto relative grid grid-cols-1 md:grid-cols-12 gap-4 px-2">
+            <div className="sm:mt-28 md:col-span-4">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Technologies;
